811a07b9555da4a78e2e4f2ea2f59e4097379094
krittika-28 <krittikavermaabstract0603@gmail.com>
2026-05-21 15:38:45 +0530

    clean: remove duplicate code from database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,3 @@
-/* const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-pool.on('connect', () => {
-  console.log('Connected to PostgreSQL database!');
-});
-
-pool.on('error', (err) => {
-  console.error('Database error:', err.message);
-});
-
-module.exports = pool; */
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
